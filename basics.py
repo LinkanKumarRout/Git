7b8984f40e30ae5574c9c16519156629ed459ef1
Linkan Kumar Rout <linkankumarrout@gmail.com>
2024-05-28 20:57:52 +0530

    Added basics.py file without adding it to staging area

---
+++ basics.py
@@ -0,0 +1,27 @@
+# All important git commands
+
+#1. git init
+'''
+This initialize git on a repository.
+'''
+#2. git add file_name
+'''
+it will add that file to the staging area.
+'''
+#3. git add .
+'''
+It will add every file present on present working directory to the staging area.
+'''
+#4. git status
+'''
+This command is used to check the status of git. i.e. it will check is there any changes made to files inside a git repository.
+'''
+#5. git commit -m "message"
+'''
+This command will save all the changes, means all those file those are in the staging area. it's just like saving a file. We can add desired message to staged files.
+'''
+#6. git commit -am "message"
+'''
+This will commit files without adding it to the staging area.
+'''
+#7. 
